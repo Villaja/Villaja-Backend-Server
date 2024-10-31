896571bf359fb8d7db9a59a9d998227e85bd4119
Daniel Fasiku <adewolefas@gmail.com>
2024-11-01 00:21:36 +0100

    server changes

--- db/Database.js
+++ db/Database.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-const app = require("../app");
 
 const connectDatabase = async () => {
   try {
@@ -9,24 +8,6 @@ const connectDatabase = async () => {
     });
     
     console.log(`MongoDB connected: ${conn.connection.host}`);
-
-    // create server
-const server = app.listen(process.env.PORT, () => {
-  console.log(
-    `Server is running on http://localhost:${process.env.PORT}`
-  );
-});
-
-// unhandled promise rejection
-process.on("unhandledRejection", (err) => {
-  console.log(`Shutting down the server for ${err.message}`);
-  console.log(`shutting down the server for unhandle promise rejection`);
-
-  server.close(() => {
-    process.exit(1);
-  });
-});
-
   } catch (error) {
     console.error(error);
     process.exit(1);
