22bad6468a4076055d4aeae1beab78b2692d79eb
Daniel Fasiku <adewolefas@gmail.com>
2024-11-01 01:05:38 +0100

    fix

--- server.js
+++ server.js
@@ -22,21 +22,22 @@ cloudinary.config({
   cloud_name: process.env.CLOUDINARY_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET
-});
+})
 
-// create server
-const server = app.listen(process.env.PORT || 8000, () => {
-  console.log(
-    `Server is running on http://localhost:${process.env.PORT || 8000}`
-  );
-});
 
-// unhandled promise rejection
-process.on("unhandledRejection", (err) => {
-  console.log(`Shutting down the server for ${err.message}`);
-  console.log(`shutting down the server for unhandle promise rejection`);
+// // create server
+// const server = app.listen(process.env.PORT, () => {
+//   console.log(
+//     `Server is running on http://localhost:${process.env.PORT}`
+//   );
+// });
 
-  server.close(() => {
-    process.exit(1);
-  });
-});
+// // unhandled promise rejection
+// process.on("unhandledRejection", (err) => {
+//   console.log(`Shutting down the server for ${err.message}`);
+//   console.log(`shutting down the server for unhandle promise rejection`);
+
+//   server.close(() => {
+//     process.exit(1);
+//   });
+// });
